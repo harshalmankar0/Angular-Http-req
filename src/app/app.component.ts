1ec267fcadd80c19578515fbda12ae6e25047240
Harshal Mankar <harshalmankar52@gmail.com>
2021-08-18 18:06:46 +0530

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
+import { PostsService } from './post.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'http-requests';
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
+
+  constructor(private http: HttpClient, private postService: PostsService) {}
+
+  ngOnInit() {
+    this.isFetching = true;
+    this.postService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error =>{
+      this.error = error.message;
+    });
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.postService.createAndStorePost(postData.title, postData.content)
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.isFetching = true;
+    this.postService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error =>{
+      this.error = error.message;
+    });
+  }
+
+  onClearPosts() {
+    // Send Http request
+    this.postService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
+  }
+}
